c3333a9280167ee59d4ec2061a126cd885fe818a
Donald Shen <825870831@qq.com>
2020-07-07 16:20:21 +0800

    feat: styleguide

---
+++ docs/basic.md
@@ -0,0 +1,15 @@
+基本用法
+
+```vue
+<template>
+  <v-gantt/>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+```
---
+++ styleguide.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const glob = require('glob')
+
+module.exports = {
+  pagePerSection: true,
+  ribbon: {
+    url: 'https://github.com/FEMessage/v-gantt',
+  },
+  sections: [
+    {
+      name: 'Components',
+      components: 'src/index.vue',
+    },
+    {
+      name: 'Demo',
+      sections: glob
+        .sync('docs/*.md')
+        .map((p) => ({ name: path.basename(p, '.md'), content: p })),
+    },
+  ],
+  // webpackConfig: {
+  //   // custom config goes here
+  // },
+  exampleMode: 'expand',
+}
