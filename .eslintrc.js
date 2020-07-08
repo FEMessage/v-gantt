a832e6136326b47b6fb565d68d9efc34253893ac
Donald Shen <825870831@qq.com>
2020-07-08 15:05:34 +0800

    build: 内联 svg

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,11 @@ module.exports = {
       libraryExport: 'default',
     },
   },
+  chainWebpack: (config) => {
+    // 内联图片
+    // TODO: 考察 vue-svg-loader
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule.use('svg-inline-loader').loader('svg-inline-loader')
+  },
 }
