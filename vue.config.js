35a2d3d53250e82c3e9894bdb30df579defa07c6
Donald Shen <825870831@qq.com>
2020-07-08 16:20:27 +0800

    perf: external 资源，250 -> 140 kb

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,11 @@ module.exports = {
     output: {
       libraryExport: 'default',
     },
+    externals: {
+      axios: 'commonjs axios',
+      dayjs: 'commonjs dayjs',
+      'lodash.clonedeep': 'commonjs lodash.clonedeep',
+      store2: 'commonjs store2',
+    },
   },
 }
