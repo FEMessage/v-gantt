86219b350677397e22a06a4727f9aeb30bc299c9
Donald Shen <825870831@qq.com>
2020-07-09 14:46:54 +0800

    build: husky

---
+++ stylelint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  extends: 'stylelint-config-standard',
+  rules: {
+    'no-empty-source': null,
+    'declaration-colon-newline-after': null,
+    'no-descending-specificity': null,
+    'value-list-comma-newline-after': null,
+  },
+}
