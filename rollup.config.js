90a1697c6f514279dbcd5a2f6eb2606f6c34e6fd
Donald Shen <825870831@qq.com>
2020-07-09 14:10:26 +0800

    docs: 补充 rollup 打包说明

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,13 @@
+/**
+ * 除了使用 vue-cli（webpack）打包，项目同样支持使用 rollup 打包。
+ * 建议优先使用 webpack 打包的代码（体积更小，vue-cli 有细致的优化）
+ * rollup 可以作为产出 esm 模块的补充
+ */
+
 // 相比 @rollup/plugin-typescript，解决了引入虚拟模块的问题
 // https://github.com/ezolenko/rollup-plugin-typescript2/issues/78#issuecomment-399524537
 import typescript from 'rollup-plugin-typescript2'
-// rollup-plugin-vue 依赖很多 commonjs 模块
 import commonjs from '@rollup/plugin-commonjs'
-// 版本控制在 ^5；6.0.0-beta 无法识别 css block
 import vue from 'rollup-plugin-vue'
 import bundleSize from 'rollup-plugin-bundle-size'
 import { terser } from 'rollup-plugin-terser'
@@ -30,5 +34,13 @@ export default {
     'lodash.clonedeep',
     'store2',
   ],
-  plugins: [commonjs(), typescript(), vue(), bundleSize(), terser()],
+  plugins: [
+    // rollup-plugin-vue 依赖很多 commonjs 模块
+    commonjs(),
+    typescript(),
+    // 版本控制在 ^5；6.0.0-beta 无法识别 css block
+    vue(),
+    bundleSize(),
+    terser(),
+  ],
 }
