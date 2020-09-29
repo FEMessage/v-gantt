8756be838d128cb5599407b3fb27a7198a267215
gd4ark <gd.4ark@gmail.com>
2020-09-29 15:42:06 +0800

    chore: docs directory

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,7 +4,7 @@ const glob = require('glob')
 
 module.exports = {
   title: 'v-gantt 文档',
-  styleguideDir: 'docs', // 每次构建会清除掉内容，所以文档不能放在里面
+  styleguideDir: 'docs',
   version: require('./package.json').version,
   pagePerSection: true,
   ribbon: {
@@ -20,12 +20,33 @@ module.exports = {
     {
       name: 'Demo',
       sections: glob
-        .sync('docs-md/*.md')
+        .sync('docs/*.md')
         .map((p) => ({ name: path.basename(p, '.md'), content: p })),
     },
   ],
-  // webpackConfig: {
-  //   // custom config goes here
-  // },
   exampleMode: 'expand',
+  dangerouslyUpdateWebpackConfig(webpackConfig) {
+    /**
+     * @see https://vue-styleguidist.github.io/Configuration.html#dangerouslyupdatewebpackconfig
+     *
+     * 官方不推荐使用这个方法，因为随意修改 webpack 配置可能导致 styleguidist 无法正常运行
+     * 但由于每次构建都删除 docs 下的所有文件，且不支持自定义 CleanWebpackPlugin 选项
+     * 最终只能采用这种方式，使它构建时不删除 .md 文件
+     */
+    if (webpackConfig.plugins) {
+      for (const plugin of webpackConfig.plugins) {
+        const isCleanWebpackPlugin = 'cleanOnceBeforeBuildPatterns' in plugin
+
+        if (!isCleanWebpackPlugin) continue
+
+        const isCleanAll = plugin.cleanOnceBeforeBuildPatterns.includes('**/*')
+
+        if (isCleanAll) {
+          plugin.cleanOnceBeforeBuildPatterns.push('!*.md')
+        }
+      }
+    }
+
+    return webpackConfig
+  },
 }
